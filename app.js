97e452b5181bd330b836492f020ef4eed89f2157
Jack Agtual <jaagtual@gmail.com>
2022-09-16 17:41:41 -0700

    Fixing spelling errors and error handling in playRound function

--- app.js
+++ app.js
@@ -7,25 +7,43 @@ const playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
 
-    const winStr = `You win!`
-    const loseStr = `You lose`
-    const tieStr = `Cat's game`
+    const rock = 'rock';
+    const paper = 'paper';
+    const scissors = 'scissors';
+    
+    // check inputs
+    if (computerSelection !== rock || 
+        computerSelection !== paper ||
+        computerSelection !== scissors) {
+            console.error(`Invalid computer selection`);
+            return 'err';
+    }
+    if (playerSelection !== rock || 
+        playerSelection !== paper ||
+        playerSelection !== scissors) {
+            console.error(`Invalid player selection`);
+            return 'err';
+    }
 
-    if (playerSelection === 'rock'){
-        if      (computerSelection === 'rock')  return tieStr
-        else if (computerSelection === 'paper') return loseStr
-        else                                    return winStr
+    const winStr = `You win!`;
+    const loseStr = `You lose`;
+    const tieStr = `Cat's game`;
 
-    } else if (playerSelection === 'paper'){
-        if      (computerSelection === 'rock')  return winStr
-        else if (computerSelection === 'paper') return tieStr
-        else                                    return loseStr
+    if (playerSelection === rock){
+        if      (computerSelection === rock)   return tieStr
+        else if (computerSelection === paper) return loseStr
+        else                                   return winStr
+
+    } else if (playerSelection === paper){
+        if      (computerSelection === rock)   return winStr
+        else if (computerSelection === paper) return tieStr
+        else                                   return loseStr
 
     } else {
         // player selects scissors
-        if      (computerSelection === 'rock')  return loseStr
-        else if (computerSelection === 'paper') return winStr
-        else                                    return tieStr
+        if      (computerSelection === rock)  return loseStr
+        else if (computerSelection === paper) return winStr
+        else                                  return tieStr
     }
 }
 
@@ -39,7 +57,8 @@ const game = numGames => {
 // add event listener to player's buttons
 const playerButtons = document.querySelectorAll('.selection.you');
 playerButtons.forEach(btn => {
-    btn.addEventListener('click', 
-        () => console.log(btn.innerText)
-    )
+    const playerMove = btn.innerText;
+    btn.addEventListener('click', () => {
+        console.log(playRound(playerMove, 'paper'))
+    });
 })
